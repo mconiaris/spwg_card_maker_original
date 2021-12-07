c4227377a8cc197b91d86d9102c05b13597e0776
mconiaris <mconiaris@gmail.com>
2021-12-07 12:41:24 -0500

    Added csv file and code to read it to seed Wrestlers.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
 Wrestler.destroy_all
+
+CSV.foreach(Rails.root.join('lib/Original_Cards.csv'), headers: true) do |row|
+  
+  Wrestler.create({
+    name: row[0]
+    set: row[1]
+    pri_s: row[2]
+    pri_tt: row[3]
+    tt: row[4]
+    card_rating: row[5]
+    oc_prob: row[6]
+    total_points: row[7]
+    dq_prob: row[8]
+    pa_prob: row[9]
+    sub_prob: row[10]
+    xx_prob: row[11]
+   	submission: row[12]
+    tag_team_save: row[13]
+  })
+end
+
 

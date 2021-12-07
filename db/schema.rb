3bdc63f28174485b8ef8a497996bffbc689cec6f
mconiaris <mconiaris@gmail.com>
2021-12-07 12:01:18 -0500

    Migrated database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_12_07_165524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "wrestlers", force: :cascade do |t|
+    t.string "name"
+    t.string "set"
+    t.integer "pri_s"
+    t.integer "pri_tt"
+    t.float "tt"
+    t.float "card_rating"
+    t.float "oc_prob"
+    t.float "total_points"
+    t.float "dq_prob"
+    t.float "pa_prob"
+    t.float "sub_prob"
+    t.float "xx_prob"
+    t.float "submission"
+    t.float "tag_team_save"
+    t.string "division"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end

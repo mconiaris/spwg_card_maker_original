e63dba6134d592357b67f19b329a08d3a1b3133f
mconiaris <mconiaris@gmail.com>
2021-12-07 12:43:55 -0500

    Addeded commas to fix error in CSV file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,19 +12,19 @@ Wrestler.destroy_all
 CSV.foreach(Rails.root.join('lib/Original_Cards.csv'), headers: true) do |row|
   
   Wrestler.create({
-    name: row[0]
-    set: row[1]
-    pri_s: row[2]
-    pri_tt: row[3]
-    tt: row[4]
-    card_rating: row[5]
-    oc_prob: row[6]
-    total_points: row[7]
-    dq_prob: row[8]
-    pa_prob: row[9]
-    sub_prob: row[10]
-    xx_prob: row[11]
-   	submission: row[12]
+    name: row[0],
+    set: row[1],
+    pri_s: row[2],
+    pri_tt: row[3],
+    tt: row[4],
+    card_rating: row[5],
+    oc_prob: row[6],
+    total_points: row[7],
+    dq_prob: row[8],
+    pa_prob: row[9],
+    sub_prob: row[10],
+    xx_prob: row[11],
+   	submission: row[12],
     tag_team_save: row[13]
   })
 end

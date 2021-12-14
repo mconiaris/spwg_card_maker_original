1329eca4ab3e7104f69f03402f944c07d8b6b8d1
mconiaris <mconiaris@gmail.com>
2021-12-14 15:13:19 -0500

    Created more CRUD methods for westler.

--- app/controllers/wrestlers_controller.rb
+++ app/controllers/wrestlers_controller.rb
@@ -6,4 +6,20 @@ class WrestlersController < ApplicationController
   def show
     @wrestler = Wrestler.find(params[:id])
   end
+
+  def new
+    @wrestler = Wrestler.new
+  end
+
+  def create
+    @wrestler = Wrestler.new
+
+    if @wrestler.save
+      redirect_to @wrestler
+    else
+      render :new
+    end
+  end
+
+
 end

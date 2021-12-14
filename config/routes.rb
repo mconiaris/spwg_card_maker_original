ac9e88db07652a80b4679fc9d464fa4abf4d8f38
mconiaris <mconiaris@gmail.com>
2021-12-14 13:16:04 -0500

    Replaced specific routes for wrestler with the resources method. This gives us all of the conventional routes with less code.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   root 'wrestlers#index'
 
-  get 'wrestlers/index'
-
-  get "/wrestlers", to: "wrestlers#index"
-
-  get "/wrestlers/:id", to: "wrestlers#show"
+  resources :wrestlers
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
